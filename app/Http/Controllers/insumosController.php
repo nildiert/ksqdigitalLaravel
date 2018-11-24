a6af373b5fa0a85e826ad00145d603633b99d418
Nildiert <niljordan23@gmail.com>
2018-11-23 23:36:02 -0500

    creado el controlador de insumos 23112018_2335

---
+++ app/Http/Controllers/insumosController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Insumo;
+use Illuminate\Http\Request;
+
+class insumosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return 'estas en el index, hdspm';
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Insumos  $insumos
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Insumos $insumos)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Insumos  $insumos
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Insumos $insumos)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Insumos  $insumos
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Insumos $insumos)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Insumos  $insumos
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Insumos $insumos)
+    {
+        //
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,108 +1,66 @@
+
+
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html>
+
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-
-    <!-- Styles -->
-
+        <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+            
+                <!-- CSRF Token -->
+                <meta name="csrf-token" content="{{ csrf_token() }}">
+            
+                <title>{{ config('app.name', 'Laravel') }}</title>
+            
+                <!-- Scripts -->
+                <script src="{{ asset('js/app.js') }}" defer></script>
+            
+                <!-- Fonts -->
+                <link rel="dns-prefetch" href="//fonts.gstatic.com">
+            
+                <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+                <link href="css/style.css" rel="stylesheet" type="text/css">
+                <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+            
+                <!-- Styles -->
+            
+            </head>
 </head>
-<body>
+
+<body onload="myFunction()" style="margin:0;" >
+
+        
+
+    <div class="wrapper">
+        <!-- Sidebar  -->
+        <nav id="sidebar">
+                <div class="sidebar-header">
+                <a href="{{Route('home')}}">   <h3>{{ config('app.name', 'Laravel') }}</h3></a>
+                </div>
+
+                <ul class="list-unstyled components">
+                    <p>Operaciones</p>{{--Cambiar dependiendo del rol--}}
+
+                    <li class="active">
+                        <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Insumos</a>
+                        <ul class="collapse list-unstyled" id="homeSubmenu">
+                            <li>
+                                <a href="{{Route('insumos.index')}}">Ver</a>
+                            </li>
+                            <li>
+                            <a href="">Agregar</a>
+                            </li>
+
+                        </ul>
+                    </li>
 
 
-        <div id="app">
-                <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-                    <div class="container">
-                        <a class="navbar-brand" href="{{ url('/') }}">
-                            {{ config('app.name', 'Laravel') }}
-                        </a>
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <!-- Left Side Of Navbar -->
-                            <ul class="navbar-nav mr-auto">
-
-                            </ul>
-
-                            <!-- Right Side Of Navbar -->
-                            <ul class="navbar-nav ml-auto">
-                                <!-- Authentication Links -->
-                                @guest
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        @if (Route::has('register'))
-                                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                        @endif
-                                    </li>
-                                @else
-                                    <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link " href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                            <a class="dropdown-item" href="{{ route('logout') }}"
-                                               onclick="event.preventDefault();
-                                                             document.getElementById('logout-form').submit();">
-                                                {{ __('Logout') }}
-                                            </a>
-
-                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                                @csrf
-                                            </form>
-                                        </div>
-                                    </li>
-                                @endguest
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-
-
-
-
-
-                <main class="py-4">
-                    @yield('content')
-                </main>
-            </div>
-
-        <div class="wrapper">
-
-
-                <!-- Sidebar  -->
-                <nav id="sidebar">
-                    <div class="sidebar-header">
-                    <a href="{{Route('home')}}">   <h3>{{ config('app.name', 'Laravel') }}</h3></a>
-                    </div>
-
-                    <ul class="list-unstyled components">
-                        <p>Operaciones</p>{{--Cambiar dependiendo del rol--}}
-
-                        <li class="active">
-                            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Insumos</a>
-                            <ul class="collapse list-unstyled" id="homeSubmenu">
+                    <li class="active">
+                            <a href="#habilidadesSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Productos</a>
+                            <ul class="collapse list-unstyled" id="habilidadesSubmenu">
                                 <li>
-                                    <a href="">Ver</a>
+                                <a href="">Ver</a>
                                 </li>
                                 <li>
                                 <a href="">Agregar</a>
@@ -111,56 +69,114 @@
                             </ul>
                         </li>
 
+                        <li>
 
-                        <li class="active">
-                                <a href="#habilidadesSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Productos</a>
-                                <ul class="collapse list-unstyled" id="habilidadesSubmenu">
-                                    <li>
-                                        <a href="">Ver</a>
-                                    </li>
-                                    <li>
-                                    <a href="">Agregar</a>
-                                    </li>
-
-                                </ul>
+                        <a href="#proyectosSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Proovedores</a>
+                        <ul class="collapse list-unstyled" id="proyectosSubmenu">
+                            <li>
+                            <a href="">Ver</a>
                             </li>
 
                             <li>
-
-                            <a href="#proyectosSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Proovedores</a>
-                            <ul class="collapse list-unstyled" id="proyectosSubmenu">
-                                <li>
-                                <a href="">Ver</a>
-                                </li>
-
-                                <li>
-                                    <a href="">Agregar</a>
-                                </li>
+                                <a href="">Agregar</a>
+                            </li>
 
 
-                            </ul>
-                            <a href="#asignacionSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Ordenes de compra</a>
-                            <ul class="collapse list-unstyled" id="asignacionSubmenu">
-                                <li>
-                                <a href="">Ver</a>
-                                </li>
-                                <li>
-                                    <a href="">Agregar</a>
-                                </li>
+                        </ul>
+                        <a href="#asignacionSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Ordenes de compra</a>
+                        <ul class="collapse list-unstyled" id="asignacionSubmenu">
+                            <li>
+                            <a href="">Ver</a>
+                            </li>
+                            <li>
+                                <a href="">Agregar</a>
+                            </li>
 
-                            </ul>
-
-
-                        </li>
-
-                    </ul>
+                        </ul>
 
 
+                    </li>
 
-                </nav>
+                </ul>
+          
 
 
+         
+            </nav>
 
+        <!-- Page Content  -->
+        <div id="content">
+
+            <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+                <div class="container-fluid">
+
+                    <button type="button" id="sidebarCollapse" class="btn btn-info">
+                        <i class="fas fa-align-left"></i>
+                        <span></span>
+                    </button>
+
+{{--AUTENTICACION--}}
+<ul class="navbar-nav ml-auto">
+        <!-- Authentication Links -->
+        @guest
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+            </li>
+            <li class="nav-item">
+                @if (Route::has('register'))
+                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                @endif
+            </li>
+        @else
+            <li class="nav-item ">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    {{ Auth::user()->name }} <span class="caret"></span>
+                </a>
+
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                                      document.getElementById('logout-form').submit();">
+                         {{ __('Cerrar sesión') }}
+                     </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </div>
+            </li>
+        @endguest
+    </ul>
+
+{{--FIN AUTENTICACIÓN--}}                        
+
+                </div>
+            </nav>
+
+            <div class="content">
+                @yield('content')
+            </div>
+        </div>
+    </div>
+</div>
+
+    <!-- jQuery CDN - Slim version (=without AJAX) -->
+
+
+    <script>
+            var myVar;
+
+            
+            
+            function myFunction() {
+                myVar = setTimeout(showPage, 100);
+            }
+            
+            function showPage() {
+              document.getElementById("loader").style.display = "none";
+              document.getElementById("myDiv").style.display = "block";
+            }
+            </script>
 
     <script type="text/javascript">
         $(document).ready(function () {
@@ -176,5 +192,12 @@
         });
     </script>
 
+
 </body>
+
 </html>
+
+
+
+            
+
